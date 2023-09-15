ba65a19d42437a795fb2b0bef66a1ebf3ba4a207
samppasaarinen <samppa22000@student.hamk.fi>
2023-09-15 11:32:59 +0300

    Email send

---
+++ Email.py
@@ -0,0 +1,26 @@
+
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = "projecthamk4@gmail.com"
+email_password = "brno ykpj pltt ithx"
+email_receiver = "receiver Email address"
+
+subject = "Check  out your feedback"
+body= """
+Go check out your feedback. Click the link
+"""
+
+
+em = EmailMessage()
+em["From"] = email_sender
+em["To"] = email_receiver
+em["Subject"] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
