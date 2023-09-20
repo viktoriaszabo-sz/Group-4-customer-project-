4113b61da0388a87e3c2129f18bcf56a87f746b1
viktoriaszabo-sz <szaboviktoria.sz03@gmail.com>
2023-09-20 12:47:35 +0300

    now output in only for the latest row from the excel sheet

--- trial with python/feedback_backend.py
+++ trial with python/feedback_backend.py
@@ -9,107 +9,111 @@ df = daWithExcel.df
     #   sum_of_psych    = psychological flexibility
     #   sum_of_sr       = self-reflection (like self-compasssion / self-critisim)
 
-for index, row in df.iterrows():
-    sum_of_learning = row['Sum of learning']
-    sum_of_support = row['Sum of support']
-    sum_of_competence = row['Sum of competence']
-    sum_of_filler = row['Sum of filler']
- 
-    sum_of_se = row['Sum of self-efficacy']
-    sum_of_psych = row['Sum of psychological flexibility']
-    sum_of_burnout = row['Sum of burnout']
-    sum_of_sr = row['Sum of self-reflection']
 
-    # Check LEARNING category sum
-    if sum_of_learning <= 60 and sum_of_learning > 40:
-        #subcategory 1
-        print(f"{index + 1}: Deep approach: ")
-    elif sum_of_learning <= 40 and sum_of_learning > 20: 
-        #subcategory 2
-        print(f"{index + 1}: Organized studying: ")
-    elif sum_of_learning <= 20:
-        #subcategory 3
-        print(f"{index + 1}: Surface approach: ")
+# Access the latest row in the DataFrame (latest response)
+latest_response = df.iloc[-1]
+
+# Extract the relevant values from the latest response
+sum_of_learning = latest_response['Sum of learning']
+sum_of_support = latest_response['Sum of support']
+sum_of_competence = latest_response['Sum of competence']
+sum_of_filler = latest_response['Sum of filler']
+sum_of_se = latest_response['Sum of self-efficacy']
+sum_of_psych = latest_response['Sum of psychological flexibility']
+sum_of_burnout = latest_response['Sum of burnout']
+sum_of_sr = latest_response['Sum of self-reflection']
 
 
-    # Check SUPPORT category sum
-    if sum_of_support <= 165 and sum_of_support > 110:
-        #subcategory 1
-        print(f"{index + 1}: Felt supported: ")
-    elif sum_of_support <= 110 and sum_of_support > 55: 
-        #subcategory 2
-        print(f"{index + 1}: Felt somewhat supported: ")
-    elif sum_of_support <= 55:
-        #subcategory 3
-        print(f"{index + 1}: Felt unsupported: ")
+# Check LEARNING category sum
+if sum_of_learning <= 60 and sum_of_learning > 40:
+    #subcategory 1
+    print("Deep approach: ")
+elif sum_of_learning <= 40 and sum_of_learning > 20: 
+    #subcategory 2
+    print("Organized studying: ")
+elif sum_of_learning <= 20:
+    #subcategory 3
+    print("Surface approach: ")
 
 
-    # Check COMPETENCE category sum
-    if sum_of_competence <= 75 and sum_of_competence > 50:
-        #subcategory 1
-        print(f"{index + 1}: Felt competent: ")
-    elif sum_of_competence <= 50 and sum_of_competence > 25: 
-        #subcategory 2
-        print(f"{index + 1}: Felt somewhat competent: ")
-    elif sum_of_competence <= 25:
-        #subcategory 3
-        print(f"{index + 1}: Felt incompetent: ")
+# Check SUPPORT category sum
+if sum_of_support <= 165 and sum_of_support > 110:
+    #subcategory 1
+    print("Felt supported: ")
+elif sum_of_support <= 110 and sum_of_support > 55: 
+    #subcategory 2
+    print("Felt somewhat supported: ")
+elif sum_of_support <= 55:
+    #subcategory 3
+    print("Felt unsupported: ")
 
 
-    #check FILLER category sum 
-    if sum_of_filler <= 85 and sum_of_filler > 42:
-        #subcategory 1
-        print(f"{index + 1}: Good in career, good objectives: ")
-    elif sum_of_filler <= 42 and sum_of_filler > 0: 
-        #subcategory 2
-        print(f"{index + 1}: Somewhat good objectives: ")
-    elif sum_of_filler == 0:
-        #subcategory 3
-        print(f"{index + 1}: BAd objectives, need more improvement: ")
+# Check COMPETENCE category sum
+if sum_of_competence <= 75 and sum_of_competence > 50:
+    #subcategory 1
+    print("Felt competent: ")
+elif sum_of_competence <= 50 and sum_of_competence > 25: 
+    #subcategory 2
+    print("Felt somewhat competent: ")
+elif sum_of_competence <= 25:
+    #subcategory 3
+    print("Felt incompetent: ")
 
 
-    #CHECK FOR WELLBEING - SUBCATEGORIES 
+#check FILLER category sum 
+if sum_of_filler <= 85 and sum_of_filler > 42:
+    #subcategory 1
+    print("Good in career, good objectives: ")
+elif sum_of_filler <= 42 and sum_of_filler > 0: 
+    #subcategory 2
+    print("Somewhat good objectives: ")
+elif sum_of_filler == 0:
+    #subcategory 3
+    print("Bad objectives, need more improvement: ")
+
+
+#CHECK FOR WELLBEING - SUBCATEGORIES 
         
-    #check for SELF-EFFICIANCY sum 
-    if sum_of_se <= 35 and sum_of_se > 24:
-        #subcategory 1
-        print(f"{index + 1}: very self-efficiant ")
-    elif sum_of_se <= 24 and sum_of_se > 13: 
-        #subcategory 2
-        print(f"{index + 1}: Somewhat self-efficiant ")
-    elif sum_of_se <= 13:
-        #subcategory 3
-        print(f"{index + 1}: not self-efficiant ")
+#check for SELF-EFFICIANCY sum 
+if sum_of_se <= 35 and sum_of_se > 24:
+    #subcategory 1
+    print("very self-efficiant ")
+elif sum_of_se <= 24 and sum_of_se > 13: 
+    #subcategory 2
+    print("Somewhat self-efficiant ")
+elif sum_of_se <= 13:
+    #subcategory 3
+    print("not self-efficiant ")
 
-    #check for PSYCHOLOGICAL FLEXIBILITY sum 
-    if sum_of_psych <= 35 and sum_of_psych > 24:
-        #subcategory 1
-        print(f"{index + 1}: very psychologically flexible ")
-    elif sum_of_psych <= 24 and sum_of_psych > 13: 
-        #subcategory 2
-        print(f"{index + 1}: Somewhat psychologically flexible")
-    elif sum_of_psych <= 13:
-        #subcategory 3
-        print(f"{index + 1}: not psychologically flexible")
+#check for PSYCHOLOGICAL FLEXIBILITY sum 
+if sum_of_psych <= 35 and sum_of_psych > 24:
+    #subcategory 1
+    print("very psychologically flexible ")
+elif sum_of_psych <= 24 and sum_of_psych > 13: 
+    #subcategory 2
+    print("Somewhat psychologically flexible")
+elif sum_of_psych <= 13:
+    #subcategory 3
+    print("not psychologically flexible")
     
-    #check for BURNOUT
-    if sum_of_burnout <= 45 and sum_of_burnout > 30:
-        #subcategory 1
-        print(f"{index + 1}: very burnout")
-    elif sum_of_burnout <= 30 and sum_of_burnout > 15: 
-        #subcategory 2
-        print(f"{index + 1}: Somewhat burnout")
-    elif sum_of_burnout <= 15:
-        #subcategory 3
-        print(f"{index + 1}: no burnout")
+#check for BURNOUT
+if sum_of_burnout <= 45 and sum_of_burnout > 30:
+    #subcategory 1
+    print("very burnout")
+elif sum_of_burnout <= 30 and sum_of_burnout > 15: 
+    #subcategory 2
+    print("Somewhat burnout")
+elif sum_of_burnout <= 15:
+    #subcategory 3
+    print("no burnout")
 
-    #check for SELF-REFLECTION
-    if sum_of_sr <= 5 and sum_of_sr > 0:
-        #subcategory 1
-        print(f"{index + 1}: good self-reflection")
-    elif sum_of_sr == 0: 
-        #subcategory 2
-        print(f"{index + 1}: Somewhat okay self-reflection")
-    elif sum_of_sr < 0: 
-        #subcategory 3
-        print(f"{index + 1}: bad self-reflection")
+#check for SELF-REFLECTION
+if sum_of_sr <= 5 and sum_of_sr > 0:
+    #subcategory 1
+    print("good self-reflection")
+elif sum_of_sr == 0: 
+    #subcategory 2
+    print("Somewhat okay self-reflection")
+elif sum_of_sr < 0: 
+    #subcategory 3
+    print("bad self-reflection")
