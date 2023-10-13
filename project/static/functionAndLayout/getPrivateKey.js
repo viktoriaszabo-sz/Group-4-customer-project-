940ea860320907dd06412f27e0b77b8e39bd88db
Quang Luong <luonghuyquang092022@gmail.com>
2023-10-13 08:49:02 +0300

    private key

---
+++ project/static/functionAndLayout/getPrivateKey.js
@@ -0,0 +1,46 @@
+        // Function to generate a random string of given length
+        function generateRandomString(length) {
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            let result = '';
+            for (let i = 0; i < length; i++) {
+                const randomIndex = Math.floor(Math.random() * characters.length);
+                result += characters.charAt(randomIndex);
+            }
+            return result;
+        }
+
+        // Function to generate a formatted random string
+        function generateFormattedRandomString() {
+            const currentDate = new Date();
+            const year = currentDate.getFullYear().toString().slice(-2);
+            const month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+            const day = ('0' + currentDate.getDate()).slice(-2);
+            const hours = ('0' + currentDate.getHours()).slice(-2);
+            const minutes = ('0' + currentDate.getMinutes()).slice(-2);
+            const seconds = ('0' + currentDate.getSeconds()).slice(-2);
+            const randomString = generateRandomString(8);
+
+            return `${year}${month}${day}${hours}${minutes}${seconds}${randomString}@hamk.fi`;
+        }
+
+        // Function to generate and display the random string
+        function generateAndDisplayRandomString() {
+            const randomString = generateFormattedRandomString();
+            const randomStringElement = document.getElementById('randomString');
+            randomStringElement.innerText = `${randomString}`;
+            // Copy the random string to clipboard
+            copyToClipboard(randomString);
+        }
+
+        // Function to copy text to clipboard
+        function copyToClipboard(text) {
+            const el = document.createElement('textarea');
+            el.value = text;
+            document.body.appendChild(el);
+            el.select();
+            document.execCommand('copy');
+            document.body.removeChild(el);
+        }
+
+        // Call the generateAndDisplayRandomString() function when the window has finished loading
+        window.onload = generateAndDisplayRandomString;
